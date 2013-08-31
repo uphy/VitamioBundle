0ba967c5cd4023981df3a14564fc34aa2bbbbf19
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2013-08-31 21:02:34 +0900

    Add deploy script.

---
+++ vitamio/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+version=4.1.0
+
+deploy(){
+mvn deploy:deploy-file \
+    -Dfile=libs/$3 \
+    -DrepositoryId=uphyjp-thirdparty \
+    -Durl=http://uphy.jp/nexus/content/repositories/thirdparty/ \
+    -DartifactId=vitamio-vinit \
+    -DgroupId=io.vov.vitamio \
+    -Dversion=$1 \
+    -Dclassifier=$2 \
+    -Dpackaging=so
+}
+
+deploy $version "ARMv5" "armeabi/libvinit.so"
+deploy $version "ARMv7" "armeabi-v7a/libvinit.so"
+deploy $version "x86" "x86/libvinit.so"
+
+mvn clean deploy -PARMv5
+mvn clean deploy -PARMv7
+mvn clean deploy -Px86
